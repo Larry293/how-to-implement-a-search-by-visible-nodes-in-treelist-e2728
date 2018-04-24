46a19871f7b53aa9d6ddedfbc89099dcd362b9e2
DevExpressExampleBot <fakeEmail>
2018-04-25 00:34:05 +0300

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -1,94 +1,93 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace WindowsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace WindowsApplication1
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Dim serializableAppearanceObject1 As New DevExpress.Utils.SerializableAppearanceObject()
-			Dim serializableAppearanceObject2 As New DevExpress.Utils.SerializableAppearanceObject()
-			Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
-			Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
-			Me.buttonEdit1 = New DevExpress.XtraEditors.ButtonEdit()
-			CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.panelControl1.SuspendLayout()
-			CType(Me.buttonEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' treeList1
-			' 
-			Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.treeList1.Location = New System.Drawing.Point(0, 43)
-			Me.treeList1.Name = "treeList1"
-			Me.treeList1.Size = New System.Drawing.Size(572, 439)
-			Me.treeList1.TabIndex = 0
-			' 
-			' panelControl1
-			' 
-			Me.panelControl1.Controls.Add(Me.buttonEdit1)
-			Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
-			Me.panelControl1.Location = New System.Drawing.Point(0, 0)
-			Me.panelControl1.Name = "panelControl1"
-			Me.panelControl1.Padding = New System.Windows.Forms.Padding(10)
-			Me.panelControl1.Size = New System.Drawing.Size(572, 43)
-			Me.panelControl1.TabIndex = 1
-			' 
-			' buttonEdit1
-			' 
-			Me.buttonEdit1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.buttonEdit1.EditValue = "ame1"
-			Me.buttonEdit1.Location = New System.Drawing.Point(12, 12)
-			Me.buttonEdit1.Name = "buttonEdit1"
-			Me.buttonEdit1.Properties.AutoHeight = False
-			Me.buttonEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Left, "Prev", -1, True, True, True, DevExpress.XtraEditors.ImageLocation.MiddleCenter, Nothing, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, "", Nothing, Nothing, True), New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Right, "Next", -1, True, True, False, DevExpress.XtraEditors.ImageLocation.MiddleCenter, Nothing, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject2, "", Nothing, Nothing, True)})
-'			Me.buttonEdit1.Properties.ButtonClick += New DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(Me.buttonEdit1_Properties_ButtonClick);
-			Me.buttonEdit1.Size = New System.Drawing.Size(548, 19)
-			Me.buttonEdit1.TabIndex = 0
-'			Me.buttonEdit1.EditValueChanged += New System.EventHandler(Me.buttonEdit1_EditValueChanged);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(572, 482)
-			Me.Controls.Add(Me.treeList1)
-			Me.Controls.Add(Me.panelControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.panelControl1.ResumeLayout(False)
-			CType(Me.buttonEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim serializableAppearanceObject1 As New DevExpress.Utils.SerializableAppearanceObject()
+            Dim serializableAppearanceObject2 As New DevExpress.Utils.SerializableAppearanceObject()
+            Me.treeList1 = New DevExpress.XtraTreeList.TreeList()
+            Me.panelControl1 = New DevExpress.XtraEditors.PanelControl()
+            Me.buttonEdit1 = New DevExpress.XtraEditors.ButtonEdit()
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.panelControl1.SuspendLayout()
+            CType(Me.buttonEdit1.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' treeList1
+            ' 
+            Me.treeList1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.treeList1.Location = New System.Drawing.Point(0, 43)
+            Me.treeList1.Name = "treeList1"
+            Me.treeList1.Size = New System.Drawing.Size(572, 439)
+            Me.treeList1.TabIndex = 0
+            ' 
+            ' panelControl1
+            ' 
+            Me.panelControl1.Controls.Add(Me.buttonEdit1)
+            Me.panelControl1.Dock = System.Windows.Forms.DockStyle.Top
+            Me.panelControl1.Location = New System.Drawing.Point(0, 0)
+            Me.panelControl1.Name = "panelControl1"
+            Me.panelControl1.Padding = New System.Windows.Forms.Padding(10)
+            Me.panelControl1.Size = New System.Drawing.Size(572, 43)
+            Me.panelControl1.TabIndex = 1
+            ' 
+            ' buttonEdit1
+            ' 
+            Me.buttonEdit1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.buttonEdit1.EditValue = "ame1"
+            Me.buttonEdit1.Location = New System.Drawing.Point(12, 12)
+            Me.buttonEdit1.Name = "buttonEdit1"
+            Me.buttonEdit1.Properties.AutoHeight = False
+            Me.buttonEdit1.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() { _
+                New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Left, "Prev", -1, True, True, True, DevExpress.XtraEditors.ImageLocation.MiddleCenter, Nothing, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject1, "", Nothing, Nothing, True), _
+                New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Right, "Next", -1, True, True, False, DevExpress.XtraEditors.ImageLocation.MiddleCenter, Nothing, New DevExpress.Utils.KeyShortcut(System.Windows.Forms.Keys.None), serializableAppearanceObject2, "", Nothing, Nothing, True) _
+            })
+            Me.buttonEdit1.Size = New System.Drawing.Size(548, 19)
+            Me.buttonEdit1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(572, 482)
+            Me.Controls.Add(Me.treeList1)
+            Me.Controls.Add(Me.panelControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.treeList1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.panelControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.panelControl1.ResumeLayout(False)
+            CType(Me.buttonEdit1.Properties, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private treeList1 As DevExpress.XtraTreeList.TreeList
-		Private panelControl1 As DevExpress.XtraEditors.PanelControl
-		Private WithEvents buttonEdit1 As DevExpress.XtraEditors.ButtonEdit
-	End Class
+        Private treeList1 As DevExpress.XtraTreeList.TreeList
+        Private panelControl1 As DevExpress.XtraEditors.PanelControl
+        Private WithEvents buttonEdit1 As DevExpress.XtraEditors.ButtonEdit
+    End Class
 End Namespace
 
--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -10,43 +9,42 @@ Imports DevExpress.XtraTreeList.Nodes
 Imports DevExpress.XtraTreeList
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
-		Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Name", GetType(String))
-			tbl.Columns.Add("ID", GetType(Integer))
-			tbl.Columns.Add("Number", GetType(Integer))
-			tbl.Columns.Add("Date", GetType(DateTime))
-			tbl.Columns.Add("ParentID", GetType(Integer))
-			For i As Integer = 0 To RowCount - 1
-				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i + 1, 3 - i, DateTime.Now.AddDays(i), i Mod 3 })
-			Next i
-			Return tbl
-		End Function
+    Partial Public Class Form1
+        Inherits Form
 
-		Private helper As MyTreeListSearchHelper
+        Private Function CreateTable(ByVal RowCount As Integer) As DataTable
+            Dim tbl As New DataTable()
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Date", GetType(Date))
+            tbl.Columns.Add("ParentID", GetType(Integer))
+            For i As Integer = 0 To RowCount - 1
+                tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i + 1, 3 - i, Date.Now.AddDays(i), i Mod 3 })
+            Next i
+            Return tbl
+        End Function
 
-		Public Sub New()
+        Private helper As MyTreeListSearchHelper
+
+        Public Sub New()
             InitializeComponent()
+            treeList1.DataSource = CreateTable(300)
             helper = New MyTreeListSearchHelper(treeList1)
-			treeList1.DataSource = CreateTable(300)
-			helper.FieldName = "Name"
-			helper.Text = buttonEdit1.Text
-		End Sub
+            helper.FieldName = "Name"
+            helper.Text = buttonEdit1.Text
+        End Sub
 
-		Private Sub buttonEdit1_Properties_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs) Handles buttonEdit1.Properties.ButtonClick
-			PerformSearch(e.Button.Index = 1)
-		End Sub
-		Private Sub PerformSearch(ByVal forward As Boolean)
-			helper.PerformSearch(forward)
-		   treeList1.FocusedNode = helper.CurrentNode
-		End Sub
+        Private Sub buttonEdit1_Properties_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs) Handles buttonEdit1.Properties.ButtonClick
+            PerformSearch(e.Button.Index = 1)
+        End Sub
+        Private Sub PerformSearch(ByVal forward As Boolean)
+            helper.PerformSearch(forward)
+           treeList1.FocusedNode = helper.CurrentNode
+        End Sub
 
         Private Sub buttonEdit1_EditValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles buttonEdit1.EditValueChanged
-            If Not helper Is Nothing Then
-                helper.Text = buttonEdit1.Text
-            End If
+            helper.Text = buttonEdit1.Text
         End Sub
-	End Class
+    End Class
 End Namespace
